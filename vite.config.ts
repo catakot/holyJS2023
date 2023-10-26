4c5a2190876425063c00940b1604429de8c36ada
Alexander Kot <yacat@yandex-team.ru>
2023-10-27 00:30:35 +0300

    Test deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/holyJS2023/',
   plugins: [react()],
+  build: {
+    outDir: './docs',
+  },
 });
